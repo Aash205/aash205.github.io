cacae2af33c6fb417b202c9a08aa3be05455bea5
Aash205 <aashwork11@gmail.com>
2025-09-13 17:25:58 +0530

    content add

--- src/config.ts
+++ src/config.ts
@@ -10,30 +10,58 @@ export const siteConfig = {
     github: "https://github.com/aash205",
   },
   aboutMe:
-    "Embedded Engineer with 2+ years of hands-on experience in designing, developing, and debugging firmware for a wide range of microcontrollers, including STM32, Arduino, Raspberry Pi, NRF, and ESP platforms. Skilled at programming, problem-solving, collaborating across teams, and delivering solutions from concept to deployment.",
-  skills: ["C", "C++", "Python"],
+    "Embedded Engineer with 2+ years of hands-on experience in designing, developing, and debugging firmware for a wide range of microcontrollers, including STM32, Raspberry Pi, NRF, Arduino and ESP platforms. Skilled at programming, problem-solving, collaborating across teams, and delivering solutions from concept to deployment.",
+  skills: ["C", "C++", "Python", "Linux"],
   projects: [
     {
-      name: "AI Dev Roundup Newsletter",
+      name: "Scalp Cooling System",
       description:
-        "One concise email. Five minutes. Every Tuesday. Essential AI news & trends, production-ready libraries, powerful AI tools, and real-world code examples",
+        "Developed a medical cooling device to reduce chemotherapy-induced hair loss by maintaining scalp temperature and flow control for two patients.",
       // link: "https://aidevroundup.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
+      skills: ["STM32", "ADC", "RTOS" , "UART"],
       photos: []
     },
     {
-      name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+      name: "Modular Recording System",
       description:
-        "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+        "Built a low-power system for wireless treatment recording and streaming with efficient battery monitoring and power control.",
       link: "https://fullstackextensions.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
+      skills: ["STM32", "LOW-POWER", "ADC"],
     },
     {
-      name: "ExtensionKit",
+      name: "Keto Breath Analyzer",
       description:
-        "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
+        "Created a portable breath analyzer mapping sensor voltages to CO₂ and acetone levels with real-time OLED display.",
       link: "https://extensionkit.io/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
+      skills: ["ATMEGA328p", "VOC Sensors", "I2C"],
+    },
+     {
+      name: "Smart Spray Scheduler",
+      description:
+        "Designed a programmable system for controlled gas spraying with cycle automation and Bluetooth-based remote access.",
+      link: "https://extensionkit.io/?ref=devportfolio",
+      skills: ["nRF5840", "Bluetooth", "SPI"],
+    },
+      {
+      name: "Uroflowmetry and Urodynamics",
+      description:
+        "Prototyped a non-invasive diagnostic system using acoustic sensing for accurate uroflowmetry and urodynamic measurements.",
+      link: "https://extensionkit.io/?ref=devportfolio",
+      skills: ["ATMEGA328p", "Signal Acquisition", "I2C"],
+    },
+    {
+      name: "Wireless Surveillance Rover using DTMF Technology",
+      description:
+        "Built a rover for remote data collection with live-video streaming, GPS, and autonomous obstacle detection for autonomous navigation.",
+      link: "https://extensionkit.io/?ref=devportfolio",
+      skills: ["ATMEGA328p", "Rpi", "DTMF"],
+    },
+    {
+      name: "Automated Smart Trolley",
+      description:
+        "Developed an RFID-based smart cart that identifies products and generates automated billing in real time.",
+      link: "https://extensionkit.io/?ref=devportfolio",
+      skills: ["ATMEGA328p", "RFID", "LCD"],
     },
   ],
   experience: [
@@ -42,29 +70,36 @@ export const siteConfig = {
       title: "Embedded Engineer",
       dateRange: "June 2023 - Present",
       bullets: [
-        "Led development of microservices architecture serving 1M+ users",
-        "Reduced API response times by 40% through optimization",
-        "Mentored team of 5 junior developers",
+        "Contributed to various medical and embedded projects, taking them from concept design to functional prototype",
+        "Applied STM32, Arduino, NRF, Bluetooth, RTOS, and various sensor technologies to build prototypes with real-world clinical and IoT applications.",
+        "Collaborated with clients and cross-functional teams (mechanical, industrial, and software) to define timelines and workflow architecture",
       ],
     },
     {
-      company: "Startup Inc",
-      title: "Full Stack Developer",
-      dateRange: "Jun 2020 - Dec 2021",
+      company: "One Accord",
+      title: "Embedded Intern",
+      dateRange: "January 2023 – April 2023",
       bullets: [
-        "Built and launched MVP product from scratch using React and Node.js",
-        "Implemented CI/CD pipeline reducing deployment time by 60%",
-        "Collaborated with product team to define technical requirements",
+        "Designed a digital pregnancy test kit using STM32, a photosensor for light intensity detection, and an OLED display for result visualization.",
+        "Focused on accurate sensing, seamless hardware integration, and compact, user-friendly design.",
       ],
     },
     {
-      company: "Digital Agency",
-      title: "Frontend Developer",
-      dateRange: "Aug 2018 - May 2020",
+      company: "Embedos Engineering LPP",
+      title: "Embedded Intern",
+      dateRange: "June 2022 – July 2022",
       bullets: [
-        "Developed responsive web applications for 20+ clients",
-        "Improved site performance scores by 35% on average",
-        "Introduced modern JavaScript frameworks to legacy codebases",
+        "Worked on developing the ESP8266 Bluetooth NimBLE stack to enhance wireless communication features.",
+        "Gained experience in integrating and modifying third-party libraries for embedded applications.",
+      ],
+    },
+    {
+      company: "Emertex",
+      title: "Embedded Intern",
+      dateRange: "Sep 2021 – Nov 2021",
+      bullets: [
+        "Implemented embedded C concepts by simulating a washing machine program on PIC16F877A.",
+        "Used CLCD, tactile keys, and timers on MPLAB for functional prototyping and testing",
       ],
     },
   ],
@@ -75,8 +110,10 @@ export const siteConfig = {
       dateRange: "2019 - 2023",
       achievements: [
         "Graduated with a CGPA of 8.6",
-        "Coordinator of Technical Council IETE, Placement Cell and Alumni Cell",
-        // "President of Computer Science Club",
+        "Presented LY Paper ar ICAETIS - 2023",
+        "Student Co-ordinator | TPO KJSCE",
+        "Creative Head, Joint Treasurer | IETE KJSCE",
+        "Networking and PR Team Member | Alumini Cell KJSCE"
       ],
     },
   ],
